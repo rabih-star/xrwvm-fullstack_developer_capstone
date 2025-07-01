451b90d87360d8bcfb3a04b4a156241c03576a0f
rabih-star <63723362+rabih-star@users.noreply.github.com>
2025-07-01 15:40:03 +0300

    Add files via upload

--- server/database/eslint.config.mjs
+++ server/database/eslint.config.mjs
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
+import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
 
 
@@ -8,4 +9,5 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
   pluginReact.configs.flat.recommended,
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
 ]);
